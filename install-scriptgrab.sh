1d6b3994fca6b31f9ebf30d4eea3ad48d7390407
Rays-Robotics <rayplays.real@gmail.com>
2025-01-14 20:52:57 -0500

    Update install-scriptgrab.sh

--- install-scriptgrab.sh
+++ install-scriptgrab.sh
@@ -8,11 +8,18 @@ INSTALL_DIR="/usr/local/bin"
 SCRIPT_NAME="scriptgrab"
 
 # Download ScriptGrab
-wget https://github.com/Rays-Robotics/ScriptGrab/raw/main/scriptgrab-v1 -O "$INSTALL_DIR/$SCRIPT_NAME"
+wget https://github.com/Rays-Robotics/ScriptGrab/raw/refs/heads/main/scriptgrab-v2.sh -O "$INSTALL_DIR/$SCRIPT_NAME"
 
 # Make it executable
 chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
 
-echo "ScriptGrab has been installed! You can now use it by typing: scriptgrab"
+# Confirm installation
+if [[ -f "$INSTALL_DIR/$SCRIPT_NAME" ]]; then
+    echo "ScriptGrab v2 has been successfully installed!"
+    echo "You can now use it by typing: scriptgrab"
+else
+    echo "Installation failed. Please check your permissions or try again."
+fi
+
 
 
